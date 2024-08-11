157316cbe975bddd001495e9e03606df9fc8c814
Ânderson Q <anquadros@gmail.com>
2024-08-11 15:00:05 -0300

    Updates to serverless

--- app.py
+++ app.py
@@ -1,33 +1,26 @@
 import os
 from flask import Flask, render_template, request, jsonify, send_from_directory
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
 from textblob import TextBlob
 import random
 import json
+import string
 
 app = Flask(__name__)
-
-# Download NLTK data
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('averaged_perceptron_tagger')
 
 # Load Shakespeare quotes
 with open('shakespeare.json', 'r') as f:
     quotes = json.load(f)
 
-lemmatizer = WordNetLemmatizer()
-stop_words = set(stopwords.words('english'))
+# Simple tokenization function
+def simple_tokenize(text):
+    return text.lower().translate(str.maketrans('', '', string.punctuation)).split()
+
+# Simple stopwords list
+STOPWORDS = set(['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"])
 
 def preprocess(text):
-    tokens = word_tokenize(text.lower())
-    tokens = [lemmatizer.lemmatize(token) for token in tokens if token.isalnum()]
-    tokens = [token for token in tokens if token not in stop_words]
-    return tokens
+    tokens = simple_tokenize(text)
+    return [token for token in tokens if token not in STOPWORDS]
 
 def get_sentiment(text):
     return TextBlob(text).sentiment.polarity
